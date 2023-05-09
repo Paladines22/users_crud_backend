ecbd40461e23dccdd67d902be517683956caec1b
ValentinaPaladines <val.paladinezunal@gmail.com>
2023-05-09 12:40:06 -0500

    info update

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,43 +1,40 @@
-const catchError = require('../utils/catchError');
-const User = require('../models/user');
+const catchError = require("../utils/catchError");
+const User = require("../models/User");
 
-const getAll = catchError(async(req, res) => {
-    const users = await User.findAll();
-    return res.json(users);
+const getAll = catchError(async (req, res) => {
+  const users = await User.findAll();
+  return res.json(users);
 });
 
-const create = catchError(async(req, res) => {
-    const users = await User.create(req.body);
-    return res.status(201).json(users);
+const create = catchError(async (req, res) => {
+  const users = await User.create(req.body);
+  return res.status(201).json(users);
 });
 
-const getOne = catchError(async(req, res) => {
-    const { id } = req.params;
-    const user = await User.findByPk(id);
-    if(!user) return res.sendStatus(404);
-    return res.json(user);
+const getOne = catchError(async (req, res) => {
+  const { id } = req.params;
+  const user = await User.findByPk(id);
+  if (!user) return res.sendStatus(404);
+  return res.json(user);
 });
 
-const remove = catchError(async(req, res) => {
-    const { id } = req.params;
-    await User.destroy({ where: {id} });
-    return res.sendStatus(204);
+const remove = catchError(async (req, res) => {
+  const { id } = req.params;
+  await User.destroy({ where: { id } });
+  return res.sendStatus(204);
 });
 
-const update = catchError(async(req, res) => {
-    const { id } = req.params;
-    const user = await User.update(
-        req.body,
-        { where: {id}, returning: true }
-    );
-    if(user[0] === 0) return res.sendStatus(404);
-    return res.json(user[1][0]);
+const update = catchError(async (req, res) => {
+  const { id } = req.params;
+  const user = await User.update(req.body, { where: { id }, returning: true });
+  if (user[0] === 0) return res.sendStatus(404);
+  return res.json(user[1][0]);
 });
 
 module.exports = {
-    getAll,
-    create,
-    getOne,
-    remove,
-    update
-}
+  getAll,
+  create,
+  getOne,
+  remove,
+  update,
+};
